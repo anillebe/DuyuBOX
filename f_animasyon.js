93177fd7ad5ef59ad653c8f9d19c211aa6c67180
anillebe <anillebe33@gmail.com>
2025-08-04 15:15:10 +0300

    Add files via upload

--- f_animasyon.js
+++ f_animasyon.js
@@ -1,14 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form1 = document.getElementById("profilForm");
   const anim = document.getElementById("gecis");
-
-  if (form1) {
-    form1.addEventListener("submit", function(e) {
-      e.preventDefault();
-      anim.style.display = "block";
-      setTimeout(() => {
-        window.location.href = "veli_kutu.html";
-      }, 2500);
-    });
+  if (anim) {
+    anim.style.display = "none";
   }
 });
